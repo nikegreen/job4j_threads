7e941798c7d7f3eecd391d63c2c741aafd032b95
nike z <nikegreen@yandex.ru>
2022-05-21 15:16:06 +0500

    3. Прерывание нити [#1019 #303310]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,14 +5,16 @@ public class ConsoleProgress implements Runnable {
 
     @Override
     public void run() {
-        int index = 0;
-        while (!Thread.currentThread().isInterrupted()) {
-            System.out.print("\r load: " + process[index & 3]);
-            index++;
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                break;
+        boolean loop = true;
+        while (loop) {
+            for (char ch:process) {
+                System.out.print("\r load: " + ch);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    loop = false;
+                    break;
+                }
             }
         }
     }
@@ -20,11 +22,7 @@ public class ConsoleProgress implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        try {
-            Thread.sleep(8000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(8000);
         progress.interrupt();
         progress.join(600);
     }
