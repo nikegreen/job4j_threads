e26a8a2fa1817f618fd9524ad75a2a98255badf9
nike z <nikegreen@yandex.ru>
2022-05-21 19:18:37 +0500

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,27 @@
+package ru.job4j.linked;
+
+/**
+ *  Immutable class
+ *  all field final
+ *  all field is not change
+ *  remove setValue() & setNext()
+ * @param <T>
+ */
+public class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(final T value, final Node<T> next) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
+
