0adb171bf036194a463b4bbb403879855c928738
nike z <nikegreen@yandex.ru>
2022-05-25 21:43:26 +0500

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,24 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int temp;
+        int ref;
+        do {
+            ref = count.get();
+            temp = ref + 1;
+        } while (!count.compareAndSet(ref, temp));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
+
