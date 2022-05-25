4b7d1ef58d11bc2555d03af0b7cc68e70cb1cff6
nike z <nikegreen@yandex.ru>
2022-05-25 17:45:03 +0500

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,46 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    try {
+                        while (true) {
+                            System.out.println(queue.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                    System.out.println("consumer finish");
+                }
+        );
+        consumer.start();
+        final Thread producer = new Thread(
+                () -> {
+                    try {
+                        for (int index = 0; index != 3; index++) {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("producer finish");
+                }
+        );
+        producer.start();
+        try {
+            producer.join();
+            consumer.interrupt();
+        } catch (InterruptedException e) {
+        }
+        System.out.println("finish");
+    }
+}
