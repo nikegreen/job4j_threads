e003f8b450c876c8cf5a816e70827d7f5f2c74cd
nike z <nikegreen@yandex.ru>
2022-05-29 21:40:12 +0500

    4. CompletableFuture [#361626 #306730]

--- src/test/java/ru/job4j/pools/RolColSumTest.java
+++ src/test/java/ru/job4j/pools/RolColSumTest.java
@@ -2,13 +2,11 @@ package ru.job4j.pools;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
 
 public class RolColSumTest {
 
@@ -20,7 +18,6 @@ public class RolColSumTest {
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}
         };
-        //<[Sums{rowSum=10, colSum=28}, Sums{rowSum=26, colSum=32}, Sums{rowSum=42, colSum=36}, Sums{rowSum=58, colSum=40}]>
         final RolColSum.Sums[] res = {
                 new RolColSum.Sums(10, 28),
                 new RolColSum.Sums(26, 32),
@@ -28,7 +25,6 @@ public class RolColSumTest {
                 new RolColSum.Sums(58, 40)
         };
         final List<RolColSum.Sums> list = List.of(res);
-        //assertEquals(RolColSum.sum(matrix)..toString(), res.toString());
         assertThat(List.of(RolColSum.sum(matrix)), is(list));
     }
 
@@ -40,7 +36,6 @@ public class RolColSumTest {
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}
         };
-        //<[Sums{rowSum=10, colSum=28}, Sums{rowSum=26, colSum=32}, Sums{rowSum=42, colSum=36}, Sums{rowSum=58, colSum=40}]>
         final RolColSum.Sums[] res = {
                 new RolColSum.Sums(10, 28),
                 new RolColSum.Sums(26, 32),
@@ -48,7 +43,6 @@ public class RolColSumTest {
                 new RolColSum.Sums(58, 40)
         };
         final List<RolColSum.Sums> list = List.of(res);
-        //assertEquals(RolColSum.sum(matrix)..toString(), res.toString());
         assertThat(List.of(RolColSum.asyncSum(matrix)), is(list));
     }
 }
